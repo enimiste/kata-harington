fd320df80c686d841a46132d34e98c8d8da4be60
enimiste <nouni.elbachir@gmail.com>
2022-11-25 00:17:30 +0100

    add fields to deposit form

--- job-kata-frontend/src/app/accounts/deposit-dialog/deposit-dialog.component.ts
+++ job-kata-frontend/src/app/accounts/deposit-dialog/deposit-dialog.component.ts
@@ -1,3 +1,5 @@
+import { OperationRequest } from './../../requests/OperationRequest';
+import { AccountRequest } from './../../requests/AccountRequest';
 import { AccountDto } from './../../entities/AccountDto';
 import { HttpErrorResponse } from '@angular/common/http';
 import { OperationService } from './../../operation.service';
@@ -12,19 +14,26 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class DepositDialogComponent implements OnInit {
 
+  form: FormGroup;
   constructor(private operationService: OperationService,
     private dialogRef: MatDialogRef<DepositDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) private data: AccountDto) { }
+    @Inject(MAT_DIALOG_DATA) private data: AccountDto,
+    private fb: FormBuilder) {
+      this.form = this.fb.group({
+        amountInCents: this.fb.control([1]),
+        description: this.fb.control([""]),
+        accountNumber: this.fb.control({ value: this.data.accountNumber , disabled: true})
+      }, {}); }
 
   ngOnInit(): void {
   }
 
   onSubmit() {
     this.operationService.deposit({
-      amountInCents: 10000,
-      description: "Deposit from angular app",
+      amountInCents: this.form.controls["amountInCents"].value,
+      description: this.form.controls["description"].value,
       accountNumber: this.data.accountNumber,
-      operation: ""
+      operation: "DEPOSIT"
     }).subscribe({
       next: (accountDto) => this.dialogRef.close(accountDto),
       error: (error: HttpErrorResponse) => console.error(error.message)
--- job-kata-frontend/src/app/app.module.ts
+++ job-kata-frontend/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//*********** Angular material */
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+//*********** Angular material */
 import { NotFoundComponent } from './not-found/not-found.component';
 import { AccountsComponent } from './accounts/accounts.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -16,6 +20,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { DepositDialogComponent } from './accounts/deposit-dialog/deposit-dialog.component';
 import { WithdrawalDialogComponent } from './accounts/withdrawal-dialog/withdrawal-dialog.component';
 import { OperationHistoryComponent } from './accounts/operation-history/operation-history.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -31,12 +36,15 @@ import { OperationHistoryComponent } from './accounts/operation-history/operatio
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
     MatToolbarModule,
     MatIconModule,
     MatTableModule,
     MatButtonModule,
     MatDialogModule,
-    HttpClientModule
+    HttpClientModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
